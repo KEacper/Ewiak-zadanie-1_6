50f57e04c165604c02905039c4ba3aaeab6d545d
KEacper <101186813+KEacper@users.noreply.github.com>
2022-03-22 12:34:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    int i = 0;
+    for(i=0;i<=300;i++){
+      System.out.println("temperatura "+i+" celscjusza to "+(i*9/5+32)+" farenheita");
+    }
+  }
+}
